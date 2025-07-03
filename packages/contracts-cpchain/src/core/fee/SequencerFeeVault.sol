658b26c428841bb77151b152f1565706b9949316
guoshijiang <guoshijiang2012@163.com>
2025-07-03 18:37:14 +0800

    start pos staking

---
+++ packages/contracts-cpchain/src/core/fee/SequencerFeeVault.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { FeeVault } from "src/core/fee/FeeVault.sol";
+import { Types } from "src/libraries/Types.sol";
+
+contract SequencerFeeVault is FeeVault {
+    /// @custom:semver 0.0.1
+    string public constant version = "0.0.1";
+
+    constructor(address _recipient, uint256 _minWithdrawalAmount) FeeVault(_recipient, _minWithdrawalAmount) { }
+
+    function feeWallet() public view returns (address) {
+        return RECIPIENT;
+    }
+}
---
+++ packages/contracts-cpchain/src/libraries/Types.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title Types
+/// @notice Contains various types used throughout the Optimism contract system.
+library Types {
+    /// @notice Struct representing a withdrawal transaction.
+    /// @custom:field nonce    Nonce of the withdrawal transaction
+    /// @custom:field sender   Address of the sender of the transaction.
+    /// @custom:field target   Address of the recipient of the transaction.
+    /// @custom:field value    Value to send to the recipient.
+    /// @custom:field gasLimit Gas limit of the transaction.
+    /// @custom:field data     Data of the transaction.
+    struct WithdrawalTransaction {
+        uint256 nonce;
+        address sender;
+        address target;
+        uint256 value;
+        uint256 gasLimit;
+        bytes data;
+    }
+}
